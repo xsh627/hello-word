0f31de4631d9212bacb144d81ca10df810ac9d83
shuhongxiao <shuhongxiao@sohu-inc.com>
2018-01-27 20:29:06 +0800

    [ph-pages]-gulp配置

---
+++ build/gulpfile.js
@@ -0,0 +1,45 @@
+'use strict';
+
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let args = require('get-gulp-args')();
+
+let config = {
+	sass: {
+		src: '../sccc/',
+		dest: '../css/'
+	},
+	js: {
+		src: '../js/'
+	}
+};
+
+let WATCH_SRC = args[0];
+
+gulp.task('css:min', () => {
+	return gulp.src(config.sass.src + `${WATCH_SRC}` + '/**/*.scss')
+		.pipe(sass({
+            sourceComments: 'map',
+            outputStyle: 'nested'
+        }).on('error', sass.logError))
+		.pipe(gulp.dest(config.sass.dest + `${WATCH_SRC}`));
+});
+
+gulp.task('sass', function(){
+    //sass()方法用于转换sass到css
+  return gulp.src('../scss/*.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('../css'))
+});
+
+//Watching Sass files for changes
+gulp.task('watch', function(){
+  gulp.watch('../scss/**/*.scss', ['sass']); 
+  // Other watchers
+})
+
+//test
+gulp.task('test', function () {
+	gulp.src(config.js.src + '/**/*.js')
+		.pipe(gulp.dest(config.sass.dest));
+});
--- js/player.js
+++ js/player.js
@@ -1,3 +1,7 @@
 !function(){
-	
+	var listData;
+	var audioObj = document.createElement('audio');
+	audioObj.id = 'audioEle';
+	audioObj.src = 'http://cdn5.lizhi.fm/audio/2017/10/12/2629787855524158470_hd.mp3';
+	audioObj.play();
 }()
